3dd301d6221ae9268df3873d429567d091743d7d
iustin <iustin.neaga@cherrychain.it>
2024-05-30 18:55:39 +0200

    added content spawn

--- src/tilemap_plugin.rs
+++ src/tilemap_plugin.rs
@@ -1,6 +1,6 @@
 use std::ops::Index;
 use bevy::prelude::*;
-use robotics_lib::world::tile::TileType;
+use robotics_lib::world::tile::{Content, TileType};
 use crate::visualizer;
 use crate::visualizer::{Map, TileSize};
 
@@ -9,7 +9,8 @@ pub(crate) struct TileMapPlugin;
 impl Plugin for TileMapPlugin{
     fn build(&self, app: &mut App) {
         app
-            .add_systems(Startup, create_map);
+            .add_systems(Startup, create_map)
+            .add_systems(Startup, create_content);
     }
 }
 
@@ -31,9 +32,43 @@ fn create_map(mut commands: Commands, sprite: Res<visualizer::SpriteSheetRust>, 
                 TileType::Snow => {TextureAtlasSprite::new(7)}
                 TileType::Lava => {TextureAtlasSprite::new(8)}
                 TileType::Teleport(_) => {TextureAtlasSprite::new(9)}
-                TileType::Wall => {
-                    println!("{:?}", (i, j));
-                    TextureAtlasSprite::new(10)}
+                TileType::Wall => { TextureAtlasSprite::new(10)} };
+            imageTile.custom_size = Some(Vec2::splat(tile_size));
+            commands.spawn(SpriteSheetBundle {
+                texture_atlas: sprite.0.clone(),
+                sprite: imageTile,
+                transform: Transform::from_xyz((i as f32) * tile_size, (j as f32) * tile_size, -2.0),
+                ..Default::default()
+            });
+        }
+    }
+}
+
+
+fn create_content(mut commands: Commands, sprite: Res<visualizer::SpriteSheetRust>, map: Res<Map>, tile_size: Res<TileSize>) {
+    let world = map.as_ref().map.clone();
+    let tile_size = tile_size.as_ref().tile_size;
+
+    for i in 0.. world.len(){
+        for j in 0..world[0].len(){
+            let tile = world[i][j].content.clone();
+            let mut imageTile = match tile {
+                Content::Rock(_) => {TextureAtlasSprite::new(12)}
+                Content::Tree(_) => {TextureAtlasSprite::new(13)}
+                Content::Garbage(_) => {TextureAtlasSprite::new(14)}
+                Content::Fire => {TextureAtlasSprite::new(15)}
+                Content::Coin(_) => {TextureAtlasSprite::new(16)}
+                Content::Bin(_) => {TextureAtlasSprite::new(17)}
+                Content::Crate(_) => {TextureAtlasSprite::new(18)}
+                Content::Bank(_) => {TextureAtlasSprite::new(19)}
+                Content::Water(_) => {TextureAtlasSprite::new(27)}
+                Content::Market(_) => {TextureAtlasSprite::new(20)}
+                Content::Fish(_) => {TextureAtlasSprite::new(21)}
+                Content::Building => {TextureAtlasSprite::new(22)}
+                Content::Bush(_) => {TextureAtlasSprite::new(23)}
+                Content::JollyBlock(_) => {TextureAtlasSprite::new(24)}
+                Content::Scarecrow => {TextureAtlasSprite::new(25)}
+                Content::None => {TextureAtlasSprite::new(27)}
             };
             imageTile.custom_size = Some(Vec2::splat(tile_size));
             commands.spawn(SpriteSheetBundle {
